fe0c55aa6d6fad3212292889c28b81d8186e6823
shehryar49 <shehryarshahid49@gmail.com>
2024-05-16 00:36:52 +0500

    Save progress

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <cstdio>
 #include <iostream>
 #include <cmath>
 #include <string.h>
 #include <random>
+#include <chrono>
 
 #define EULER_NUMBER 2.718281828459045
 using namespace std;
@@ -235,7 +237,10 @@ public:
     void matmul(const Matrix& rhs,Matrix& result)const
     {
         if(cols != rhs.rows || result.rows!=rows || result.cols != rhs.cols) //multiplication not possible
-          return;
+        {
+            fprintf(stderr,"WARNING: Matrix multiplication not possible for orders (%zu,%zu) and (%zu,%zu)\n",rows,cols,rhs.rows,rhs.cols);
+            return;
+        }
         for(size_t i=0;i<rows;i++)
         {
             for(size_t j=0;j<rhs.cols;j++)
@@ -248,16 +253,17 @@ public:
         }
     }
 	//Non inplace operations or methods that allocate memory
-	Matrix repeat_cols(int n)
+	Matrix repeat_cols(size_t n)
     {
 		Matrix res(rows,cols*n);
 		for(size_t i = 0;i<rows;i++)
 		{
-			for(size_t j=0;j<n;j++)
+            size_t m = 0;
+			for(size_t j=1;j<=n;j++)
 			{
 				for(size_t k=0;k<cols;k++)
 				{
-					res.matrix[i][j*n + k] = matrix[i][k];
+					res.matrix[i][m++] = matrix[i][k];
 				}
 			}
 		}
@@ -441,23 +447,28 @@ public:
 	Callback fnd;
 	ErrorFun errfnd;
     Matrix aL;
-	Matrix* aL_dup; // auxiliary space to perform some operations on aL
+    Matrix ld;
+	Matrix aL_dup; // auxiliary space to perform some operations on aL
+    Matrix dout_dup;
+    Matrix dw_transpose;
     Layer(size_t num_neurons,size_t num_inputs,Callback activation_fn,Callback fnd,ErrorFun errfn) : weights(num_inputs,num_neurons),
-	   bias(1,num_neurons),aL(1,num_neurons)
+	   bias(1,num_neurons),aL(1,num_neurons),ld(1,1),aL_dup(1,1),dout_dup(1,1),dw_transpose(1,1)
 	{
 		//assign random weights
+
         double lower_bound = -1.0;
    		double upper_bound = 1.0;
    		std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
-   		std::default_random_engine re;
+        std::random_device rd;
+        std::default_random_engine re(rd()); 
 
         for(size_t i=0;i<num_inputs;i++)
 		{
 			for(size_t j=0;j<num_neurons;j++)
-				weights[i][j] = 1;//unif(re);
+				weights[i][j] = unif(re);
 		}
 		for(size_t i=0;i<num_neurons;i++)
-			bias[0][i] = 1;//unif(re);
+			bias[0][i] = unif(re);
 
         fn = (Callback)activation_fn;
 		this->fnd = (Callback)fnd;
@@ -468,16 +479,15 @@ public:
 		aL.resize(inputs.rows,weights.cols);
 		inputs.matmul(weights,aL);
 		aL.addrow(bias);
-		//if(fn)
-		//	fn(res,res); //activate output
+		if(fn)
+			fn(aL,aL); //activate output
 		return aL;
 	}
     // deltas at output layer
     const Matrix& deltas_output(const Matrix& y)
 	{
-		static Matrix ld(y.num_rows(),y.num_cols());
-		static Matrix aL_dup(aL.rows,aL.cols);
-        this->aL_dup = &aL_dup; 
+        ld.resize(y.rows,y.cols);
+        aL_dup.resize(aL.rows,aL.cols);
 		if(!fnd)
 			errfnd(ld,aL,y);
 		else 
@@ -491,19 +501,22 @@ public:
 	}  
     Matrix& deltas(const Matrix& dout,const Matrix& dw) // dw are weights of next layer
 	{
-        static Matrix dout_dup(dout.rows,dout.cols);
-		static Matrix transpose(dw.rows,dw.cols);
-		
-		dw.transpose(transpose);
-		dout.matmul(transpose,dout_dup);
+		dout_dup.resize(dout.rows,dw.rows);
+        dw_transpose.resize(dw.cols,dw.rows);
+        aL_dup.resize(aL.rows,aL.cols);
+
+        dw.transpose(dw_transpose);
+		dout.matmul(dw_transpose,dout_dup);
+        
 		if(fnd)
 		{
-			fnd(aL,*aL_dup);
-			dout_dup.mul(*aL_dup);
+
+			fnd(aL,aL_dup);
+			dout_dup.mul(aL_dup);
 		}
 		return dout_dup;
 	}
-    void backpropagate(const Matrix& a,const Matrix& del,size_t idx,double lr = 1)
+    void backpropagate(const Matrix& a,const Matrix& del,size_t idx,double lr = 0.1)
 	{
         // stochastic gradient descent
 		Matrix inputs(1,a.cols);
@@ -547,23 +560,19 @@ int main()
 
 	Layer hidden(2,2,&sigmoid,&sigmoid_derivative,&MSE_derivative);
 	Layer output(1,2,sigmoid,sigmoid_derivative,MSE_derivative);
-	//cout << output.weights;
-    //return 0;
-    size_t epochs = 100;
-
+    size_t epochs = 100000;
+    double loss;
 	for(size_t i=1;i<=epochs;i++)
 	{
 		const Matrix& a1 = hidden.forward(inputs);
         const Matrix& a2 = output.forward(a1);
-		//cout << a2;
-        //cout << y;
-		cout << "Loss: "<< MSE(a2,y) <<endl; 
-		//return 0;
+        loss = MSE(a2,y);
     	const Matrix& dout = output.deltas_output(y);
     	const Matrix& dh = hidden.deltas(dout,output.weights);
 		size_t idx = rand()%4; // pick a random idx
     	output.backpropagate(a1,dout,idx,0.1);
     	hidden.backpropagate(inputs,dh,idx,0.1);
 	}
-
+    cout << "Loss: "<<loss<<endl;
+    cout << output.aL;
 }
